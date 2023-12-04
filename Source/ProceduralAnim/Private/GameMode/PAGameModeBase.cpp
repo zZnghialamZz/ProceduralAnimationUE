98eaf37a198bafcea9fc6dbe3149e87f6988e2bf
Nghia Lam <nghialam12795@gmail.com>
2023-12-04 14:42:56 +0700

    Initialize project

---
+++ Source/ProceduralAnim/Private/GameMode/PAGameModeBase.cpp
@@ -0,0 +1,3 @@
+// Create by Nghia Lam <nghialam12795@gmail.com>. See LICENSE for more info.
+
+#include "GameMode/PAGameModeBase.h"
---
+++ Source/ProceduralAnim/Public/GameMode/PAGameModeBase.h
@@ -0,0 +1,13 @@
+// Create by Nghia Lam <nghialam12795@gmail.com>. See LICENSE for more info.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PAGameModeBase.generated.h"
+
+UCLASS()
+class PROCEDURALANIM_API APAGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+};
